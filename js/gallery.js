5255940bb9bff390d5690b564ff6b90541c84aaa
Harish <106601387+harish4462@users.noreply.github.com>
2023-05-10 12:16:38 +0530

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,7 +2,7 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic) {
-  document.getElementById("image").style.backgroundImage = "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon.jpg')";
+  document.getElementById("image").style.backgroundImage = "url("+ previewPic.src +")";
   document.getElementById("image").innerHTML = previewPic.alt;
     /* In this function you should
     1) change the url for the background image of the div with the id = "image"
